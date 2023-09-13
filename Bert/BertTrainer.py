0d5c466d066f429bc619054277f89d723821b5a8
mortur <mortur311@gmail.com>
2023-09-13 17:20:23 +0300

    apply t5 changes (model init bug fix, prints, results csv file to specific folder) to bert

--- Bert/BertTrainer.py
+++ Bert/BertTrainer.py
@@ -1,6 +1,6 @@
 import evaluate
 import logging
-import wandb
+# import wandb
 from transformers import (
     HfArgumentParser,
     TrainingArguments,
@@ -17,14 +17,14 @@ import pandas as pd
 from datasets import load_dataset
 from datetime import datetime
 import os
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, recall_score, precision_score
 
 import sys
 sys.path.append('../')
-from Utils.utils import DataTrainingArguments, ModelArguments, DEVICE
+from Utils.utils import DataTrainingArguments, ModelArguments, DEVICE, print_cur_time
 
 logger = logging.getLogger(__name__)
-wandb.init(project='HumorNLP')
+# wandb.init(project='HumorNLP')
 
 
 class BertTrainer:
@@ -36,6 +36,8 @@ class BertTrainer:
         self.classifier = None
         self.raw_datasets = None
         self.results = {}
+        self.results_df = None
+        self.final_results_df = None
         self.trainer = None
         self.dataset_columns = None
         self.text_column, self.label_column = None, None
@@ -43,14 +45,27 @@ class BertTrainer:
         self.train_idx = -1
         self.parser = HfArgumentParser((ModelArguments, DataTrainingArguments, TrainingArguments))
         self.model_args, self.data_args, self.training_args = self.parser.parse_args_into_dataclasses()
+        self.run_dir_name = None
 
     def pipeline(self):
+        print_cur_time('STARTING PIPELINE')
         set_seed(self.training_args.seed)
+        self.init_run_dir()
         self.load_files()
-        self.config_model()
+        self.config_and_tokenizer()
         self.set_data_attr()
         self.preprocess_datasets()
         self.train_and_predict()
+        print_cur_time('ENDING PIPELINE')
+
+    def init_run_dir(self):
+        time = datetime.now()
+        self.run_dir_name = '../Data/output/results/runs/{model_name}_{date}_{hour}_{minute}/'.format(
+            model_name=self.model_args.model_name_or_path,
+            date=time.date(),
+            hour=time.hour, minute=time.minute
+        )
+        os.makedirs(self.run_dir_name, exist_ok=True)
 
     def load_files(self):
         if (self.training_args.do_eval and 'no_val' in self.data_args.split_type) or \
@@ -96,6 +111,9 @@ class BertTrainer:
                                                                                  split_type=self.data_args.split_type,
                                                                                  split_name='test')
                     data_files[f'{dataset}_test'] = curr_predict_path
+
+            print_cur_time('Loading datasets:')
+            print(data_files)
             self.raw_datasets = load_dataset(
                 extension,
                 data_files=data_files,
@@ -103,13 +121,16 @@ class BertTrainer:
                 use_auth_token=True if self.model_args.use_auth_token else None,
             )
 
-    def config_model(self):
+    def config_and_tokenizer(self):
         self.config = BertConfig.from_pretrained(self.model_args.model_name_or_path)
         self.tokenizer = BertTokenizer.from_pretrained(self.model_args.model_name_or_path)
+
+        self.metric = evaluate.load('accuracy')
+
+    def init_model(self):
         self.model = BertForSequenceClassification.from_pretrained(self.model_args.model_name_or_path,
                                                                    config=self.config).to(DEVICE)
         self.classifier = TextClassificationPipeline(model=self.model, tokenizer=self.tokenizer, device=0)
-        self.metric = evaluate.load('accuracy')
 
     def set_data_attr(self):
         # column names for input/target
@@ -137,8 +158,10 @@ class BertTrainer:
 
     def preprocess_datasets(self):
         if self.training_args.do_train:
-            train_dataset = self.raw_datasets["train"]
+            if self.data_args.train_file:
+                train_dataset = self.raw_datasets["train"]
             if self.data_args.train_path_template is not None:
+                print_cur_time('Loading train datasets from train_path_template')
                 self.train_datasets = []
                 for dataset in self.data_args.trained_on:
                     self.train_datasets.append(self.raw_datasets[f'{dataset}_train'])
@@ -155,8 +178,10 @@ class BertTrainer:
                     # remove_columns=self.train_datasets[i].column_names,  ## all columns??)
 
         if self.training_args.do_eval:
-            eval_dataset = self.raw_datasets["validation"]
+            if self.data_args.validation_file:
+                eval_dataset = self.raw_datasets["validation"]
             if self.data_args.train_path_template is not None:
+                print_cur_time('Loading validation datasets from train_path_template')
                 self.eval_datasets = []
                 for dataset in self.data_args.trained_on:
                     self.eval_datasets.append(self.raw_datasets[f'{dataset}_validation'])
@@ -173,9 +198,11 @@ class BertTrainer:
                     # remove_columns=self.eval_datasets[i].column_names)
 
         if self.training_args.do_predict:
-            predict_dataset = self.raw_datasets["test"]
+            if self.data_args.test_file:
+                predict_dataset = self.raw_datasets["test"]
             if self.data_args.datasets_to_predict and \
                     self.data_args.test_path_template is not None:
+                print_cur_time('Loading test datasets from test_path_template')
                 self.predict_datasets = []
                 for dataset in self.data_args.datasets_to_predict:
                     self.predict_datasets.append(self.raw_datasets[f'{dataset}_test'])
@@ -193,24 +220,40 @@ class BertTrainer:
                     # remove_columns=self.predict_datasets[i].column_names)
 
     def train_and_predict(self):
+        print_cur_time('STARTING TRAINING')
+
         epochs = self.data_args.epochs
         batch_sizes = self.data_args.batch_sizes
         lrs = self.data_args.learning_rates
         seeds = self.data_args.seeds
         general_output_dir = self.training_args.output_dir
 
+        self.results_df = pd.DataFrame(columns=['dataset', 'epoch', 'batch_size', 'learning_rate',
+                                                'seed', 'accuracy'])
+
+        self.final_results_df = pd.DataFrame(columns=['model', 'trained_on', 'epoch', 'batch_size',
+                                                      'learning_rate', 'seed', 'predict_on', 'accuracy',
+                                                      'recall', 'precision'])
+
         for i in range(len(self.data_args.trained_on)):
             self.train_idx = i
+            # init results for this dataset
+            self.results = {}
+
             for ep in epochs:
                 for bs in batch_sizes:
                     for lr in lrs:
                         for seed in seeds:
                             self.set_run_details(ep, bs, lr, seed, general_output_dir)
+                            self.init_model()
                             self.train()
                             self.eval()
-                            self.predict()
+                            self.predict(ep, bs, lr, seed)
                             self.compute_performance(ep, bs, lr, seed)
-                            wandb.finish()
+                            self.results_df.to_csv(self.run_dir_name + 'models_accuracy.csv', index=False)
+                            self.final_results_df.to_csv(self.run_dir_name + 'models_performance_all.csv', index=False)
+
+                            # wandb.finish()
 
             self.save_results()
 
@@ -231,8 +274,8 @@ class BertTrainer:
             lr=self.training_args.learning_rate,
             split_type=self.data_args.split_type)
 
-        wandb.init(project='HumorNLP', name=self.training_args.run_name)
-        wandb.run.name = self.training_args.run_name
+        # wandb.init(project='HumorNLP', name=self.training_args.run_name)
+        # wandb.run.name = self.training_args.run_name
         self.training_args.output_dir = '{0}/{1}'.format(general_output_dir,
                                                          self.training_args.run_name)
 
@@ -247,6 +290,8 @@ class BertTrainer:
         )
 
         if self.training_args.do_train:
+            print_cur_time(f'START TRAIN ON {self.training_args.run_name}')
+
             train_result = self.trainer.train()
             if self.model_args.save_model:
                 self.trainer.save_model()  # Saves the tokenizer too for easy upload
@@ -264,10 +309,14 @@ class BertTrainer:
             if self.model_args.save_state:
                 self.trainer.save_state()
 
+            print_cur_time(f'END TRAIN ON {self.training_args.run_name}')
+
     def eval(self):
         # Evaluation
         if self.training_args.do_eval:
             logger.info("*** Evaluate ***")
+            print_cur_time(f'START EVAL ON {self.training_args.run_name}')
+
             if isinstance(self.eval_datasets[self.train_idx], dict):
                 metrics = {}
                 for eval_ds_name, eval_ds in self.eval_datasets[self.train_idx].items():
@@ -284,12 +333,24 @@ class BertTrainer:
             if self.model_args.save_metrics:
                 self.trainer.save_metrics("eval", metrics)
 
-    def predict(self):
+            print_cur_time(f'END EVAL ON {self.training_args.run_name}')
+
+    def predict(self, ep, bs, lr, seed):
         if self.training_args.do_predict:
             logger.info("*** Predict ***")
+            print_cur_time(f'START PREDICT OF {self.training_args.run_name}')
+
             for i in range(len(self.predict_datasets)):
-                df_real = pd.read_csv(
-                    f'../Data/humor_datasets/{self.data_args.datasets_to_predict[i]}/{self.data_args.split_type}/test.csv')
+                print_cur_time(f'START PREDICT ON {self.data_args.datasets_to_predict[i]}')
+                # df_real = pd.read_csv(
+                #     f'../Data/humor_datasets/{self.data_args.datasets_to_predict[i]}/{self.data_args.split_type}/test.csv')
+
+                df_real_path = self.data_args.test_path_template.format(
+                    dataset=self.data_args.datasets_to_predict[i], split_type=self.data_args.split_type, split_name='test'
+                )
+                print_cur_time(f'PREDICT file path  {df_real_path}')
+
+                df_real = pd.read_csv(df_real_path)
                 max_predict_samples = (
                     min(self.data_args.max_predict_samples, len(df_real))
                     if self.data_args.max_predict_samples is not None else
@@ -303,9 +364,25 @@ class BertTrainer:
                 df_pred['bert_sentence'] = df_real['bert_sentence']
                 df_pred['id'] = df_real['id']
                 df_pred['label'] = df['label'].apply(lambda s: s[-1])
+                df_pred['true_label'] = df_real['label']
 
-                cols = ['id', 'bert_sentence', 'label']
+                cols = ['id', 'bert_sentence', 'label', 'true_label']
                 df_pred = df_pred[cols]
+
+                accuracy = float("%.4f" % accuracy_score(df_real.label, df_pred.label))
+                recall = float("%.4f" % recall_score(df_real.label, df_pred.label))
+                precision = float("%.4f" % precision_score(df_real.label, df_pred.label))
+
+                row_to_final_results = {'model': self.model_args.model_name_or_path,
+                                        'trained_on': self.data_args.trained_on[self.train_idx],
+                                        'epoch': ep, 'batch_size': bs,
+                                        'learning_rate': lr, 'seed': seed,
+                                        'predict_on': self.data_args.datasets_to_predict[i],
+                                        'accuracy': accuracy, 'recall': recall, 'precision': precision}
+
+                print(row_to_final_results)
+
+                self.final_results_df = self.final_results_df.append([row_to_final_results])
 
                 os.makedirs(f'{self.training_args.output_dir}/predictions', exist_ok=True)
                 output_prediction_file = os.path.join(
@@ -313,23 +390,42 @@ class BertTrainer:
                     "{dataset}_preds.csv".format(dataset=self.data_args.datasets_to_predict[i]))
                 df_pred.to_csv(output_prediction_file, index=False)
 
+                print_cur_time(f'END PREDICT ON {self.data_args.datasets_to_predict[i]}')
+
+            print_cur_time(f'END PREDICT OF {self.training_args.run_name}')
+
     def compute_performance(self, ep, bs, lr, seed):
+        print_cur_time(f'START COMPUTE OF {self.training_args.run_name}')
         dataset_name = self.data_args.compute_on[self.train_idx]
-        df_real = pd.read_csv(f'../Data/humor_datasets/{dataset_name}/{self.data_args.split_type}/test.csv')
+        print_cur_time(f'COMPUTE dataset {dataset_name}')
+        # df_real = pd.read_csv(f'../Data/humor_datasets/{dataset_name}/{self.data_args.split_type}/test.csv')
+        df_real_path = self.data_args.test_path_template.format(
+            dataset=dataset_name, split_type=self.data_args.split_type, split_name='test'
+        )
+        print_cur_time(f'COMPUTE file path  {df_real_path}')
+        df_real = pd.read_csv(df_real_path)
+
         prediction_file = os.path.join(
             self.training_args.output_dir, 'predictions',
             "{dataset}_preds.csv".format(dataset=dataset_name))
         df_pred = pd.read_csv(prediction_file)
         df_real = df_real.iloc[:len(df_pred)]
 
-        self.results[ep, bs, lr, seed] = accuracy_score(df_real.label, df_pred.label)
+        accuracy = accuracy_score(df_real.label, df_pred.label)
+        self.results[ep, bs, lr, seed] = accuracy
+        result_to_df = {'dataset': self.data_args.trained_on[self.train_idx],
+                        'epoch': ep, 'batch_size': bs, 'learning_rate': lr,
+                        'seed': seed, 'accuracy': accuracy}
+        self.results_df = self.results_df.append([result_to_df], ignore_index=True)
+
+        print_cur_time(f'END COMPUTE OF {self.training_args.run_name}')
 
     def save_results(self):
         time = datetime.now()
-        results_file_path = '../Data/output/results/{model_name}_on_{dataset}_{date}_{hour}_{minute}.txt'.format(
+        results_file_path = '{run_dir}/{model_name}_on_{dataset}_{hour}_{minute}.txt'.format(
             model_name=self.model_args.model_name_or_path,
             dataset=self.data_args.trained_on[self.train_idx],
-            date=time.date(),
+            run_dir=self.run_dir_name,
             hour=time.hour, minute=time.minute
         )
 
